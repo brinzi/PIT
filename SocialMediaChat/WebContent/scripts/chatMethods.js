f2e6a2f2eb567241f2bce957c5b4360137be8118
brinzi <brinzoiu.bogdan@yahoo.com>
2015-05-27 23:25:25 +0300

    Made more stuff
    Change-Id: Ife5ce95d0e0e8cf8ebb5d5f10f3fc452b5df134b
    Signed-off-by: brinzi <brinzoiu.bogdan@yahoo.com>

--- SocialMediaChat/WebContent/scripts/chatMethods.js
+++ SocialMediaChat/WebContent/scripts/chatMethods.js
@@ -9,16 +9,24 @@ $(function (){
 
 	function addText(){
 		var msg=$("#userInput").val();
-		$("#display").append( $("<p></p>").text(msg));
+		$("#display").append( $("<p id=\"sentMsg\"></p>").text(msg));
+		$("#userInput").val("");
 
 	}
 	
-	$("#send").click(function() {
+	$("#send").click(function(e) {
+		e.preventDefault();
 		addText();
 	});
 		
 		
-	$("#")	
+	$("#userInput").keypress(function(e){
+	
+		if(e.which == 13) {
+			e.preventDefault();
+	       addText();
+	    }
+	});	
 	
 	
 	
